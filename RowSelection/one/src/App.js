378f16220abb14327047e2007d9af3c9721dc7b9
Anil Namde <anilnamde@gmail.com>
2018-08-02 07:05:50 +0530

    extracted state

--- RowSelection/one/src/App.js
+++ RowSelection/one/src/App.js
@@ -1,31 +1,137 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'react-tabs/style/react-tabs.css';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import {
+  Tab, Tabs, TabList, TabPanel,
+} from 'react-tabs';
 import Places from './Places';
 import People from './People';
+
+
+const names = ['Elly', 'Shane', 'Niall', 'Rob', 'John', 'Sean', 'Dicky', 'Willy', 'Shaggy', 'Spud', 'Sugar', 'Spice'];
+
+const models = [
+  'Mondeo',
+  'Celica',
+  'Boxter',
+  'Minty',
+  'Snacky',
+  'FastCar',
+  'Biscuit',
+  'Whoooper',
+  'Scoooper',
+  'Jet Blaster',
+];
+
+const categories = ['Sold', 'For Sale', 'In Workshop'];
+
+function poundFormatter(params) {
+  return (
+    `\xA3${
+      Math.floor(params.value)
+        .toString()
+        .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}`
+  );
+}
+
+function createNewRowData(category) {
+  const newData = {
+    category,
+    model: models[Math.floor(Math.random() * models.length)],
+    price: Math.floor(Math.random() * 800000) + 20000,
+    zombies: names[Math.floor(Math.random() * names.length)],
+    style: 'Smooth',
+    clothes: 'Jeans',
+    created: new Date().getTime(),
+  };
+  return newData;
+}
+
+function getInitialRowData() {
+  const rowData = [];
+  for (let i = 0; i < 12; i++) {
+    const category = categories[i % categories.length];
+    rowData.push(createNewRowData(category));
+  }
+  return rowData;
+}
+
+
+const props = {
+  columnDefs: [
+    {
+      field: 'category',
+      rowGroupIndex: 1,
+      hide: true,
+    },
+    {
+      field: 'price',
+      aggFunc: 'sum',
+      valueFormatter: poundFormatter,
+    },
+    { field: 'zombies' },
+    { field: 'style' },
+    { field: 'clothes' },
+    { field: 'created' },
+  ],
+  defaultColDef: { width: 100 },
+  groupDefaultExpanded: 1,
+  rowData: getInitialRowData(),
+  rowSelection: 'multiple',
+  getRowClass(params) {
+    const rowNode = params.node;
+    if (rowNode.group) {
+      switch (rowNode.key) {
+        case 'In Workshop':
+          return 'category-in-workshop';
+        case 'Sold':
+          return 'category-sold';
+        case 'For Sale':
+          return 'category-for-sale';
+        default:
+          return null;
+      }
+    } else {
+      return null;
+    }
+  },
+  autoGroupColumnDef: {
+    headerName: 'Group',
+    field: 'model',
+    rowGroupIndex: 1,
+    cellRenderer: 'agGroupCellRenderer',
+    cellRendererParams: { checkbox: true },
+  },
+  onGridReady(params) {
+    params.api.sizeColumnsToFit();
+  },
+};
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
         <Tabs>
-            <TabList>
-                <Tab>
+          <TabList>
+            <Tab>
                     Person
-                </Tab>
-                <Tab>
+            </Tab>
+            <Tab>
                     Places
-                </Tab>
-            </TabList>
-            <TabPanel>
-                <h2>Person</h2>
-                <People />
-            </TabPanel>
-            <TabPanel>
-                <h2>Places</h2>
-                <Places />
-            </TabPanel>
+            </Tab>
+          </TabList>
+          <TabPanel>
+            <h2>
+Person
+            </h2>
+            <People {...props} />
+          </TabPanel>
+          <TabPanel>
+            <h2>
+Places
+            </h2>
+            <Places {...props} />
+          </TabPanel>
         </Tabs>
       </div>
     );
--- RowSelection/one/src/People.js
+++ RowSelection/one/src/People.js
@@ -1,202 +1,103 @@
-import React, { Component } from "react";
-import { AgGridReact } from "ag-grid-react";
-import "ag-grid-enterprise";
+import React, { Component } from 'react';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-enterprise';
 import 'ag-grid/dist/styles/ag-grid.css';
 import 'ag-grid/dist/styles/ag-theme-balham.css';
 
 export default class extends Component {
-    constructor(props) {
-        super(props);
+  onGridReady(params) {
+    this.gridApi = params.api;
+    this.gridColumnApi = params.columnApi;
 
-        this.state = {
-            columnDefs: [
-                {
-                    field: "category",
-                    rowGroupIndex: 1,
-                    hide: true
-                },
-                {
-                    field: "price",
-                    aggFunc: "sum",
-                    valueFormatter: poundFormatter
-                },
-                { field: "zombies" },
-                { field: "style" },
-                { field: "clothes" },
-                { field: "created" }
-            ],
-            defaultColDef: { width: 100 },
-            groupDefaultExpanded: 1,
-            rowData: getInitialRowData(),
-            rowSelection: "multiple",
-            getRowClass: function(params) {
-                var rowNode = params.node;
-                if (rowNode.group) {
-                    switch (rowNode.key) {
-                        case "In Workshop":
-                            return "category-in-workshop";
-                        case "Sold":
-                            return "category-sold";
-                        case "For Sale":
-                            return "category-for-sale";
-                        default:
-                            return null;
-                    }
-                } else {
-                    return null;
-                }
-            },
-            autoGroupColumnDef: {
-                headerName: "Group",
-                field: "model",
-                rowGroupIndex: 1,
-                cellRenderer: "agGroupCellRenderer",
-                cellRendererParams: { checkbox: true }
-            },
-            onGridReady: function(params) {
-                params.api.sizeColumnsToFit();
-            }
-        };
-    }
+    params.api.sizeColumnsToFit();
+  }
 
-    onGridReady(params) {
-        this.gridApi = params.api;
-        this.gridColumnApi = params.columnApi;
+  // getRowData() {
+  //   const rowData = [];
+  //   this.gridApi.forEachNode((node) => {
+  //     rowData.push(node.data);
+  //   });
+  // }
 
-        params.api.sizeColumnsToFit();
-    }
+  // onAddRow(category) {
+  //   const rowDataItem = createNewRowData(category);
+  //   this.gridApi.updateRowData({ add: [rowDataItem] });
+  // }
 
-    getRowData() {
-        var rowData = [];
-        this.gridApi.forEachNode(function(node) {
-            rowData.push(node.data);
-        });
-        console.log("Row Data:");
-        console.log(rowData);
-    }
-    onAddRow(category) {
-        var rowDataItem = createNewRowData(category);
-        this.gridApi.updateRowData({ add: [rowDataItem] });
-    }
-    onMoveToGroup(category) {
-        var selectedRowData = this.gridApi.getSelectedRows();
-        selectedRowData.forEach(function(dataItem) {
-            dataItem.category = category;
-        });
-        this.gridApi.updateRowData({ update: selectedRowData });
-    }
-    onRemoveSelected() {
-        var selectedRowData = this.gridApi.getSelectedRows();
-        this.gridApi.updateRowData({ remove: selectedRowData });
-    }
-    render() {
-        console.log('People:render');
-        return (
-            <div style={{ width: "100%", height: "100%" }}>
-                <div style={{ height: "100%", boxSizing: "border-box" }}>
-                    <div
-                        id="myGrid"
-                        style={{
-                            boxSizing: "border-box",
-                            height: "100%",
-                            width: "100%"
-                        }}
-                        className="ag-theme-balham"
-                    >
-                        <AgGridReact
-                            columnDefs={this.state.columnDefs}
-                            defaultColDef={this.state.defaultColDef}
-                            groupDefaultExpanded={this.state.groupDefaultExpanded}
-                            rowData={this.state.rowData}
-                            rememberGroupStateWhenNewData={true}
-                            enableSorting={true}
-                            suppressRowClickSelection={true}
-                            rowSelection={this.state.rowSelection}
-                            animateRows={true}
-                            groupSelectsChildren={true}
-                            getRowClass={this.state.getRowClass}
-                            autoGroupColumnDef={this.state.autoGroupColumnDef}
-                            onGridReady={this.state.onGridReady}
-                            onGridReady={this.onGridReady.bind(this)}
-                        />
-                    </div>
-                </div>
+  // onMoveToGroup(category) {
+  //   const selectedRowData = this.gridApi.getSelectedRows();
+  //   selectedRowData.forEach((dataItem) => {
+  //     dataItem.category = category;
+  //   });
+  //   this.gridApi.updateRowData({ update: selectedRowData });
+  // }
+  //
+  // onRemoveSelected() {
+  //   const selectedRowData = this.gridApi.getSelectedRows();
+  //   this.gridApi.updateRowData({ remove: selectedRowData });
+  // }
 
-                <div>
-                    <div>
-                        <button class="bt-action" onClick={this.onAddRow.bind(this, "For Sale")}>
-                            Add For Sale
-                        </button>
-                        <button class="bt-action" onClick={this.onAddRow.bind(this, "In Workshop")}>
-                            Add In Workshop
-                        </button>
-                        <button class="bt-action" onClick={this.onRemoveSelected.bind(this)}>
-                            Remove Selected
-                        </button>
-                        <button class="bt-action" onClick={this.onMoveToGroup.bind(this, "For Sale")}>
-                            Move to For Sale
-                        </button>
-                    </div>
-                    <div style={{ marginTop: "4px" }}>
-                        <button class="bt-action" onClick={this.onMoveToGroup.bind(this, "In Workshop")}>
-                            Move to In Workshop
-                        </button>
-                        <button class="bt-action" onClick={this.onMoveToGroup.bind(this, "Sold")}>
-                            Move to Sold
-                        </button>
-                        <button class="bt-action" onClick={this.getRowData.bind(this)}>
-                            Get Row Data
-                        </button>
-                    </div>
-                </div>
-            </div>
-        );
-    }
-}
-
-function poundFormatter(params) {
+  render() {
+    console.log('People:render', this.props);
     return (
-        "\xA3" +
-        Math.floor(params.value)
-            .toString()
-            .replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
+      <div style={{ width: '100%', height: '100%' }}>
+        <div style={{ height: '100%', boxSizing: 'border-box' }}>
+          <div
+            id="myGrid"
+            style={{
+              boxSizing: 'border-box',
+              height: '100%',
+              width: '100%',
+            }}
+            className="ag-theme-balham"
+          >
+            <AgGridReact
+              columnDefs={this.props.columnDefs}
+              defaultColDef={this.props.defaultColDef}
+              groupDefaultExpanded={this.props.groupDefaultExpanded}
+              rowData={this.props.rowData}
+              rememberGroupStateWhenNewData
+              enableSorting
+              suppressRowClickSelection
+              rowSelection={this.props.rowSelection}
+              animateRows
+              groupSelectsChildren
+              getRowClass={this.props.getRowClass}
+              autoGroupColumnDef={this.props.autoGroupColumnDef}
+              onGridReady={this.props.onGridReady}
+              onGridReady={this.onGridReady.bind(this)}
+            />
+          </div>
+        </div>
+
+        {/* <div> */}
+        {/* <div> */}
+        {/* <button className="bt-action" onClick={this.onAddRow.bind(this, 'For Sale')}> */}
+        {/* Add For Sale */}
+        {/* </button> */}
+        {/* <button className="bt-action" onClick={this.onAddRow.bind(this, 'In Workshop')}> */}
+        {/* Add In Workshop */}
+        {/* </button> */}
+        {/* <button className="bt-action" onClick={this.onRemoveSelected.bind(this)}> */}
+        {/* Remove Selected */}
+        {/* </button> */}
+        {/* <button className="bt-action" onClick={this.onMoveToGroup.bind(this, 'For Sale')}> */}
+        {/* Move to For Sale */}
+        {/* </button> */}
+        {/* </div> */}
+        {/* <div style={{ marginTop: '4px' }}> */}
+        {/* <button className="bt-action" onClick={this.onMoveToGroup.bind(this, 'In Workshop')}> */}
+        {/* Move to In Workshop */}
+        {/* </button> */}
+        {/* <button className="bt-action" onClick={this.onMoveToGroup.bind(this, 'Sold')}> */}
+        {/* Move to Sold */}
+        {/* </button> */}
+        {/* <button className="bt-action" onClick={this.getRowData.bind(this)}> */}
+        {/* Get Row Data */}
+        {/* </button> */}
+        {/* </div> */}
+        {/* </div> */}
+      </div>
     );
-}
-
-function getInitialRowData() {
-    var rowData = [];
-    for (var i = 0; i < 12; i++) {
-        var category = categories[i % categories.length];
-        rowData.push(createNewRowData(category));
-    }
-    return rowData;
-}
-var names = ["Elly", "Shane", "Niall", "Rob", "John", "Sean", "Dicky", "Willy", "Shaggy", "Spud", "Sugar", "Spice"];
-
-var models = [
-    "Mondeo",
-    "Celica",
-    "Boxter",
-    "Minty",
-    "Snacky",
-    "FastCar",
-    "Biscuit",
-    "Whoooper",
-    "Scoooper",
-    "Jet Blaster"
-];
-
-var categories = ["Sold", "For Sale", "In Workshop"];
-
-function createNewRowData(category) {
-    var newData = {
-        category: category,
-        model: models[Math.floor(Math.random() * models.length)],
-        price: Math.floor(Math.random() * 800000) + 20000,
-        zombies: names[Math.floor(Math.random() * names.length)],
-        style: "Smooth",
-        clothes: "Jeans",
-        created: new Date().getTime()
-    };
-    return newData;
+  }
 }
